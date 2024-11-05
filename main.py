047cc213d449d2a45aa3b0f5547214b5daf3d2fa
Dell <jonathanphy1210@gmail.com>
2024-11-05 13:03:18 +0530

    create pdf file.

--- main.py
+++ main.py
@@ -2,3 +2,17 @@ import pandas as pd
 import glob
 from fpdf import FPDF
 from pathlib import Path
+
+filepaths = glob.glob("invoices/*.xlsx")
+
+
+for filepath in filepaths:
+
+    pdf = FPDF(orientation="P", unit='mm', format='A4')
+    pdf.add_page()
+
+
+    filename = Path(filepath).stem
+    invoice_no, date = filename.split("-")
+
+    pdf.output(f"PDFs/{filename}.pdf")
