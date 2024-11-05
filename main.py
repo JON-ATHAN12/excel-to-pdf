3b0113d70af9b4662dff80b0438e1ded949ebf37
Dell <jonathanphy1210@gmail.com>
2024-11-05 13:09:07 +0530

    install openpyxl. create table header content.

--- main.py
+++ main.py
@@ -15,4 +15,25 @@ for filepath in filepaths:
     filename = Path(filepath).stem
     invoice_no, date = filename.split("-")
 
+    pdf.set_font(family='Times', style='B', size=16)
+    pdf.cell(w=50, h=8, txt=f"Invoice no. {invoice_no}", ln=1)
+
+    pdf.set_font(family='Times', style='B', size=16)
+    pdf.cell(w=50, h=8, txt=f"Date: {date}", ln=1)
+
+    df = pd.read_excel(filepath, sheet_name='Sheet 1')
+
+    # Add Header
+    columns = df.columns
+    columns = [item.replace("_", " ").title() for item in columns]
+    pdf.set_font(family="Times", style='B', size=10)
+    pdf.set_text_color(80, 80, 80)
+    pdf.cell(w=30, h=8, txt=columns[0], border=1)
+    pdf.cell(w=60, h=8, txt=columns[1], border=1)
+    pdf.cell(w=40, h=8, txt=columns[2], border=1)
+    pdf.cell(w=30, h=8, txt=columns[3], border=1)
+    pdf.cell(w=30, h=8, txt=columns[4], border=1, ln=1)
+
+
+
     pdf.output(f"PDFs/{filename}.pdf")
